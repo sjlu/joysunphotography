a503654c9792b830786e190bd10ab4b56efe71c4
Steven Lu <tacticalazn@gmail.com>
2013-10-15 08:57:56 -0400

    Image helper.

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 __author__ = "sjlu"
 
+import imghdr
+from PIL import Image
+from os import listdir
+from os.path import isdir, isfile, join
+
 from flask import Flask, render_template
 from flask.ext.assets import Environment as Assets, Bundle
 
@@ -10,10 +15,43 @@ app = Flask(__name__)
 assets = Assets(app)
 assets.auto_build = True
 
+# Helper functions
+def list_projects():
+  path = 'static/img/projects/'
+  directories = [item for item in listdir(path) if isdir(join(path,item))]
+  directories_and_files = []
+
+  projects = []
+  for d in directories:
+    directory_path = '%s%s' % (path, d)
+    files = [f for f in listdir(directory_path) if isfile(join(directory_path, f)) and imghdr.what(join(directory_path, f))]
+
+    vertical = []
+    horizontal = []
+    for f in files:
+      image = Image.open(join(directory_path, f))
+      # if width > height
+      if image.size[0] > image.size[1]:
+        horizontal.append(f)
+      else:
+        vertical.append(f)
+
+    projects.append({
+      'name': d.replace('_', ' ').title(),
+      'path': directory_path.replace('static/', ''),
+      'files': {
+        'horizontal': horizontal,
+        'vertical': vertical
+      }
+    })
+
+  print projects
+  return projects
+
 # Define our routes.
 @app.route("/")
 def index():
-  return render_template('homepage.html')
+  return render_template('homepage.html', projects=list_projects())
 
 # Execute
 if __name__ == "__main__":
