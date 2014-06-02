6e66ad6b389c94e1317c5dedadd05539cc71f45c
Steven Lu <tacticalazn@gmail.com>
2014-06-01 22:40:58 -0400

    Fixing some bugs when dependant on fs.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var _ = require('lodash');
 
-var projects = fs.readFileSync('./build/projects.json');
+var projects = JSON.parse(fs.readFileSync('./build/projects.json', {
+  encoding: 'utf-8'
+}));
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { projects: projects });
+  console.log(projects);
 });
 
 router.get('/project/:project', function(req, res) {
