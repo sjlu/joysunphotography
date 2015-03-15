0fdde54e8822c4629970f74547c701b34e1d15bc
Steven Lu <tacticalazn@gmail.com>
2015-03-15 15:06:45 -0400

    removing b&w section

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
 var projectsGenerator = require('./lib/projects');
 var fs = require('fs');
+var _ = require('lodash');
 
 projectsGenerator(function(err, projects) {
+  projects = _.filter(projects, function(p) {
+    return !_.contains(['black_and_white'], p.canonical);
+  })
+
   fs.writeFileSync('./build/projects.json', JSON.stringify(projects));
 });
