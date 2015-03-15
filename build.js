d75f830acb3f540edac8f65f304d9dcb3351dde0
Steven Lu <tacticalazn@gmail.com>
2015-03-15 16:20:32 -0400

    new projects

--- build.js
+++ build.js
@@ -7,5 +7,11 @@ projectsGenerator(function(err, projects) {
     return !_.contains(['black_and_white'], p.canonical);
   })
 
+  _.each(projects, function(p) {
+    if (p.name === 'Nyfw') {
+      p.name = 'NYFW';
+    }
+  });
+
   fs.writeFileSync('./build/projects.json', JSON.stringify(projects));
 });
